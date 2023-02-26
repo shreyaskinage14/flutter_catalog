643bb7df496342db42534b0dc3a4520347e05e80
shreyaskinage14 <shreyaskinage14@gmail.com>
2023-02-27 00:47:28 +0530

    day 4 complete login ui done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,12 +5,48 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login Page",
-            style: TextStyle(
-                fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold)),
-      ),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "Username", hintText: "Enter Username"),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "Password", hintText: "Enter Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => {print("Hi Shreyas")},
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
