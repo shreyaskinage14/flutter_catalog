bf1129db6db5e34a01b07be95ed1c4cd58921a9e
shreyaskinage14 <shreyaskinage14@gmail.com>
2023-02-27 01:50:26 +0530

    day 6 done container animation and delay

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,8 +27,8 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 20.0,
               ),
-              const Text(
-                "Welcome",
+              Text(
+                "Welcome $name",
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
               const SizedBox(
@@ -30,9 +39,13 @@ class LoginPage extends StatelessWidget {
                     const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
                 child: Column(
                   children: [
-                    const TextField(
+                    TextField(
                       decoration: InputDecoration(
                           labelText: "Username", hintText: "Enter Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     const TextField(
                       obscureText: true,
@@ -42,14 +55,44 @@ class LoginPage extends StatelessWidget {
                     const SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      // ignore: avoid_print
-                      onPressed: () =>
-                          {Navigator.pushNamed(context, MyRoutes.homeRoute)},
-                      style: TextButton.styleFrom(
-                          minimumSize: const Size(150, 40)),
-                      child: const Text("Login"),
-                    )
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 40 : 150,
+                        height: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 8)),
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   // ignore: avoid_print
+                    //   onPressed: () =>
+                    //       {},
+                    //   style: TextButton.styleFrom(
+                    //       minimumSize: const Size(150, 40)),
+                    //   child: const Text("Login"),
+                    // )
                   ],
                 ),
               )
